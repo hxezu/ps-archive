cec3f86d2c768ac66eb0df277ab377601b6b7e03
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-09-03 20:55:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.01 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,4 @@
 function solution(arr)
 {
-    const answer = []
-    answer.push(arr[0])
-    for(let i=1; i<arr.length; i++){
-        if(arr[i-1]!==arr[i])
-            answer.push(arr[i])
-    }
-    return answer;
+    return arr.filter((val,idx)=>val!==arr[idx+1]);
 }
