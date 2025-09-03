8b941b333f3b9264d0195273a1784896d7bdab0f
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-09-03 16:04:31 +0900

    [level 2] Title: 주식가격, Time: 29.37 ms, Memory: 42 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,13 @@
+function solution(prices) {
+    var answer = [];
+    for(let i=0; i<prices.length; i++){
+         let seconds = 0;
+        for(let j=i+1; j<prices.length; j++){
+            seconds++
+            if(prices[j]<prices[i])
+                break;
+        }
+        answer.push(seconds)
+    }
+    return answer;
+}
