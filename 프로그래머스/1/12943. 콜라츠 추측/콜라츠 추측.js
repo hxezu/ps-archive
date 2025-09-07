9d014ecc64dcf683941a137e358d77b8d0a1acde
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-09-07 21:20:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    let cnt =0
+    while(num!==1){
+        if(cnt===500)
+            return -1
+        if(num%2){
+            num = num *3 +1
+        }else{
+            num /= 2
+        }
+        cnt ++
+            
+    }
+    return cnt;
+}
