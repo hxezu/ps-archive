003f5bd7899c3878740474385b1e4e690738adb6
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-09-08 17:21:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,31 +1,12 @@
 function solution(s) {
-    let answer = [];
-    let numMap = {
-        "zero":0,
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9
+    let answer = s;
+    let numbers = ["zero","one","two", "three","four","five","six","seven","eight","nine"]
+    
+    for(let i=0; i<numbers.length; i++){
+        let arr = answer.split(numbers[i])
+        answer = arr.join(i)
     }
     
-    while(s.length >0){
-        if(s[0]>="a" && s[0]<="z"){
-            for(let word in numMap){
-                if(s.startsWith(word)){
-                    answer.push(numMap[word])
-                    s=s.slice(word.length)
-                    break;
-                }
-            }
-        }else{
-            answer.push(s[0])
-            s = s.slice(1)
-        }
-    }
-    return Number(answer.join(""));
+
+    return Number(answer);
 }
