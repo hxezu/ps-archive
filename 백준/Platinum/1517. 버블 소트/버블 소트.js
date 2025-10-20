2bd7c9d08159bf2b297d344bb946d66291988b39
Hyeju Hyun <123932695+hxezu@users.noreply.github.com>
2025-10-20 14:14:58 +0900

    [Platinum V] Title: 버블 소트, Time: 972 ms, Memory: 208452 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.js
@@ -0,0 +1,42 @@
+//var input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+let swapCnt = 0;
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const result = [];
+  let i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i++]);
+    } else {
+      result.push(right[j++]);
+      swapCnt += left.length - i;
+    }
+  }
+
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+mergeSort(arr);
+
+console.log(swapCnt);
