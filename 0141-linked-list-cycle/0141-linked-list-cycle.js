e9103b271abca94d0489b5c070af1af4d59841e0
Hyeju Hyun <123932695+hxezu@users.noreply.github.com>
2025-10-21 14:09:19 +0900

    Time: 51 ms (79.7%), Space: 59.8 MB (30.1%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    const seen = new Set()
+    let node = head
+
+    while(node){
+        if(seen.has(node)) return true
+        seen.add(node)
+        node = node.next
+    }
+    return false
+};
